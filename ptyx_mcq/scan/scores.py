e18377c9ce4da5995c13c4880d855f37342abdcf
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-01-24 15:15:18 +0100

    Fix a potential bug in scores calculation when `proportional` mode was used.

--- ptyx_mcq/scan/scores.py
+++ ptyx_mcq/scan/scores.py
@@ -29,7 +29,9 @@ class ScoreData:
 
 
 def some(answers: AnswersData, score: ScoreData) -> float:
-    """Give the full score if any of the correct answers is checked while no incorrect answer is checked."""
+    """Return the maximal score if any of the correct answers and no incorrect answer is checked.
+
+     If an incorrect answer is checked, return minimal score."""
     # Answer is valid if and only if :
     # (proposed ≠ ∅ and proposed ⊆ correct) or (proposed = correct = ∅)
     ok = (answers.checked and answers.checked.issubset(answers.correct)) or (
@@ -44,7 +46,8 @@ def some(answers: AnswersData, score: ScoreData) -> float:
 
 
 def all(answers: AnswersData, score: ScoreData) -> float:
-    """Give the full score if exactly all the correct answers are checked."""
+    """Return the maximal score if all the correct answers are checked, else give the minimal score.
+    """
     ok = answers.checked == answers.correct
     if ok:
         return score.correct
@@ -55,7 +58,15 @@ def all(answers: AnswersData, score: ScoreData) -> float:
 
 
 def proportional(answers: AnswersData, score: ScoreData) -> float:
-    """Give the full score if exactly all the correct answers are checked."""
+    """Returned score is a pondered mean of minimal and maximal scores.
+
+    Minimal and maximal scores are defined respectively in the header of the ptyx file
+    with `correct` and `incorrect` keywords.
+
+    The score returned is (1 - k) * minimal_score + k * maximal_score,
+    where k is the proportion of correct answers (i.e. boxes left blank when proposed answer
+    was incorrect and boxes checked when proposed answer was correct).
+    """
     count_ok = len(answers.checked & answers.correct) + len(answers.unchecked & answers.incorrect)
     proportion = count_ok / len(answers.all)
     return round(score.incorrect + proportion * (score.correct - score.incorrect), 2)
@@ -72,22 +83,29 @@ def proportional(answers: AnswersData, score: ScoreData) -> float:
 #         return 0.5*floored_proportional(answers, score)
 
 
-def _proportion_of_correct(answers: AnswersData) -> float:
+def _checked_among_correct_proportion(answers: AnswersData) -> float:
+    """Return the proportion of checked answers among correct ones.
+
+    If there was no correct answer, return 1.0 if no answer was checked, 0.0 else.
+    """
+    if len(answers.correct) == 0:
+        return float(len(answers.checked) == 0)
     return len(answers.checked & answers.correct) / len(answers.correct)
 
 
 def partial_answers_linear(answers: AnswersData, score: ScoreData) -> float:
+    """Return 0 if an incorrect answer was checked, else the proportion of correct answers"""
     if answers.checked & answers.incorrect:
         return score.incorrect
     else:
-        return round(_proportion_of_correct(answers) * score.correct, 2)
+        return round(_checked_among_correct_proportion(answers) * score.correct, 2)
 
 
 def partial_answers_quadratic(answers: AnswersData, score: ScoreData) -> float:
     if answers.checked & answers.incorrect:
         return score.incorrect
     else:
-        return round(_proportion_of_correct(answers) ** 2**score.correct, 2)
+        return round(_checked_among_correct_proportion(answers) ** 2 ** score.correct, 2)
 
 
 # def floored_partial_anwers(answers: AnswersData, score: ScoreData) -> float:
