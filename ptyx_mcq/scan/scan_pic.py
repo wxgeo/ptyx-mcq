d25c434440f4b0a7271c4e5eafd7ceeb845224f0
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-02-11 16:23:52 +0100

    Refactoring: create `read_doc_id_and_page()` function.

--- ptyx_mcq/scan/scan_pic.py
+++ ptyx_mcq/scan/scan_pic.py
@@ -689,15 +689,46 @@ def edit_answers(m: ndarray, boxes, answered, config, doc_id, xy2ij, cell_size) 
             process = color2debug(m, wait=False)
 
 
-# def read_identifier() -> int:
-#     """Read the document ID.
-#
-#     The document ID is encoded using a homemade barcode.
-#     This code is made of a band of 16 black or white squares.
-#     (Note that the first one is always black and is only used to detect the band).
-#     Example: ■■□■□■□■■□□□■□□■ = 0b100100011010101 =  21897
-#     It allows for 2**15 = 32 768 different values.
-#     """
+def read_doc_id_and_page(m: ndarray, pos: Pixel, f_square_size: float) -> tuple[int, int]:
+    """Read the document ID and the page number.
+
+    The document ID is encoded using a homemade barcode.
+    This code is made of a band of 16 black or white squares.
+    (Note that the first one is always black and is only used to detect the band).
+    Example: ■■□■□■□■■□□□■□□■ = 0b100100011010101 =  21897
+    It allows for 2**15 = 32 768 different values.
+    """
+    square_size = round(f_square_size)
+    i, j = pos
+
+    doc_id = 0
+
+    # Test the color of the 15 following squares, and interpret it as a binary number.
+    for k in range(24):
+        j_ = round(j + (k + 1) * f_square_size)
+        if k % 2:
+            color2debug(m, (i, j_), (i + square_size, j_ + square_size), display=False)
+        else:
+            color2debug(m, (i, j_), (i + square_size, j_ + square_size), color=(0, 0, 255), display=False)
+        if test_square_color(m, i, j_, square_size, proportion=0.5, gray_level=0.5):
+            doc_id += 2 ** k
+            # ~ print((k, (i3, j)), " -> black")
+        # ~ else:
+        # ~ print((k, (i3, j)), " -> white")
+
+    # ~ color2debug(m)
+    # Nota: If necessary (although this is highly unlikely !), one may extend protocol
+    # by adding a second band (or more !), starting with a black square.
+    # This function will test if a black square is present below the first one ;
+    # if so, the second band will be joined with the first
+    # (allowing 2**30 = 1073741824 different values), and so on.
+
+    page = doc_id % 256
+    print("Page read: %s" % page)
+    doc_id = doc_id // 256
+    print("Test ID read: %s" % doc_id)
+
+    return doc_id, page
 
 
 def scan_picture(
@@ -796,47 +827,7 @@ def scan_picture(
     # ------------------------------------------------------------------
     #                      READ IDENTIFIER
     # ------------------------------------------------------------------
-
-    test_ID = 0
-    # Test the color of the 15 following squares,
-    # and interpret it as a binary number.
-
-    for k in range(24):
-        j_ = round(j + (k + 1) * f_square_size)
-        if k % 2:
-            color2debug(m, (i, j_), (i + square_size, j_ + square_size), display=False)
-        else:
-            color2debug(m, (i, j_), (i + square_size, j_ + square_size), color=(0, 0, 255), display=False)
-        if test_square_color(m, i, j_, square_size, proportion=0.5, gray_level=0.5):
-            test_ID += 2**k
-            # ~ print((k, (i3, j)), " -> black")
-        # ~ else:
-        # ~ print((k, (i3, j)), " -> white")
-
-    # ~ color2debug(m)
-    # Nota: If necessary (although this is highly unlikely !), one may extend protocol
-    # by adding a second band (or more !), starting with a black square.
-    # This function will test if a black square is present below the first one ;
-    # if so, the second band will be joined with the first
-    # (allowing 2**30 = 1073741824 different values), and so on.
-
-    page = test_ID % 256
-    print("Page read: %s" % page)
-    test_ID = test_ID // 256
-    print("Test ID read: %s" % test_ID)
-
-    # ~ yshift = i1 - pixels_per_cm
-    # ~ xshift = j1 - pixels_per_cm
-    # ~ color2debug(m, (TOP, LEFT), (TOP + square_size, LEFT + square_size))
-
-    # ~ i, j = xy2ij(10, 10) # Top-left corner
-    # ~ color2debug(m, (i, j), (i + cell_size, j + cell_size))
-
-    # ~ ii, jj = xy2ij(0, 0)
-    # ~ for x in range(0, 200, 10):
-    # ~ i, j = xy2ij(x, 287)
-    # ~ color2debug(m, (i, j), (ii, j + cell_size), display=False)
-    # ~ color2debug(m, (0, 0), (0, 0), display=True)
+    doc_id, page = read_doc_id_and_page(m, (i, j), f_square_size)
 
     # ------------------------------------------------------------------
     #                  IDENTIFY STUDENT (OPTIONAL)
@@ -974,7 +965,7 @@ def scan_picture(
     displayed_questions_numbers: Dict[int, int] = {}
     pic_data: PicData = {
         # ID of the test:
-        "ID": test_ID,  # int
+        "ID": doc_id,  # int
         # page number:
         "page": page,  # int
         "name": student_name,  # str
@@ -992,15 +983,15 @@ def scan_picture(
     }
 
     try:
-        boxes = config["boxes"][test_ID][page]
+        boxes = config["boxes"][doc_id][page]
     except KeyError:
         print(
-            f"WARNING: ID {test_ID!r} - page {page!r} not found in config file !\n"
-            f"Maybe ID {test_ID!r} - page {page!r} is an empty page ?"
+            f"WARNING: ID {doc_id!r} - page {page!r} not found in config file !\n"
+            f"Maybe ID {doc_id!r} - page {page!r} is an empty page ?"
         )
         return pic_data, m
 
-    # ordering = config['ordering'][test_ID]
+    # ordering = config['ordering'][doc_id]
     mode = config["mode"]
     #    correct_answers = config['correct_answers']
 
@@ -1030,7 +1021,7 @@ def scan_picture(
         q, a = map(int, key[1:].split("-"))
         # `q0` and `a0` keep track of apparent question and answers numbers,
         # which will be used on output to make debugging easier.
-        q0, a0 = real2apparent(q, a, config, test_ID)
+        q0, a0 = real2apparent(q, a, config, doc_id)
         displayed_questions_numbers[q] = q0
 
         # answer_is_correct = (a in correct_answers)
@@ -1047,7 +1038,7 @@ def scan_picture(
         core_blackness[(q, a)] = eval_square_color(m, i, j, cell_size, margin=7)
         positions[(q, a)] = (i, j)
 
-        answer_is_correct = is_answer_correct(q, a, config, test_ID)
+        answer_is_correct = is_answer_correct(q, a, config, doc_id)
 
         if (
             test_square(proportion=0.2, gray_level=0.65)
@@ -1143,8 +1134,8 @@ def scan_picture(
     # ~ color2debug(m, (0,0), (0,0), display=True)
     # ~ print(f'\nScore: {ANSI_REVERSE}{score:g}{ANSI_RESET}\n')
     if manual_verification is True:
-        print(test_ID, page)
-        edit_answers(m, boxes, answered, config, test_ID, xy2ij, cell_size)
+        print(doc_id, page)
+        edit_answers(m, boxes, answered, config, doc_id, xy2ij, cell_size)
     elif debug:
         color2debug(m)
     else:
