6dd2e467cfbe2b12579ca4dedb3c3945a0d16d11
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2023-10-22 16:40:51 +0200

    fix!: Fix some evaluation methods, and improve doc and tests.

--- ptyx_mcq/scan/evaluation_strategies.py
+++ ptyx_mcq/scan/evaluation_strategies.py
@@ -103,20 +103,15 @@ class EvaluationStrategies:
         The minimal and maximal scores are specified in the header of the ptyx file
         using the keywords "correct" and "incorrect", respectively.
 
+        Typically, you should set minimal_score = -maximal_score, if you want an average
+        score of zero when answering randomly.
+
         The formula for the calculated score is `(1 - k) * minimal_score + k * maximal_score`,
-        where k is the proportion of correct answers.
+        where k is the proportion of correct answers (correctly checked or unchecked boxes).
+
         This means that if all answers are correct, the score will be equal to the maximal score;
         if all answers are incorrect, the score will be equal to the minimal score;
         and if some answers are correct and others are incorrect, the score will be somewhere in between.
-
-        To determine the value of k, the algorithm looks at the answers provided and checks
-        which boxes were left blank when the proposed answer was incorrect,
-        and which boxes were checked when the proposed answer was correct.
-        The proportion of correct answers is then calculated as the ratio of the number of correct answers
-        to the total number of answers.
-
-        Overall, this function evaluates the quality of the answers provided, based on the specified
-        minimal and maximal scores, and the proportion of correct answers.
         """
         count_ok = len(answers.checked & answers.correct) + len(answers.unchecked & answers.incorrect)
         proportion = count_ok / len(answers.all)
@@ -124,51 +119,87 @@ class EvaluationStrategies:
 
     @staticmethod
     def partial_answers(answers: AnswersData, score: ScoreData) -> float:
-        """Return 0 if an incorrect answer was checked,
-        else the proportion of correct answers."""
+        """Return `score.incorrect` if an incorrect answer was checked,
+        else the proportion of correct answers multiplied by `score.correct`."""
         return _partial_answers(answers, score)
 
     @staticmethod
     def partial_answers_quadratic(answers: AnswersData, score: ScoreData) -> float:
-        """Return 0 if an incorrect answer was checked,
-        else the squared proportion of correct answers."""
+        """Same algorithm as for `partial_answers`, but the ratio is squared.
+
+        This makes more unlikely for a student answering randomly to gain significant score."""
         return _partial_answers(answers, score, exposant=2)
 
     @staticmethod
-    def legacy_correct_minus_incorrect(answers: AnswersData, score: ScoreData) -> float:
-        """If the proportion of correct answers is less than 0.5, as is typically the case,
+    def special_correct_minus_incorrect(answers: AnswersData, score: ScoreData) -> float:
+        """This algorithm is designed for multiple-choice questions that feature null scores
+        for incorrect answers.
+
+        It aims at keeping the average gain low for random answers, while not making use of negative
+        scores for incorrect answers.
+
+        This requires enough answers to work well (typically n >= 6 at least),
+        and for the student to not know the number of correct answers.
+
+        The following table gives an approximation of the average score per question
+        when answering randomly, if the maximal score = +1 and the minimal score is 0,
+        and half of the answers are correct (worst case):
+
+            ┌───────────────────┬───────┬───────┬───────┬───────┐
+            │   number of boxes │   4   │   6   │   8   │  10   │
+            ├───────────────────┼───────┼───────┼───────┼───────┤
+            │   average score   │ 0.16  │ 0.11  │ 0.09  │ 0.07  │
+            ├───────────────────┼───────┼───────┼───────┼───────┤
+            │ average score for │ 0.11  │ 0.06  │ 0.04  │ 0.03  │
+            │ quadratic version │       │       │       │       │
+            └───────────────────┴───────┴───────┴───────┴───────┘
+
+        Note that if there is only one correct answer per question, there can't be
+        partially correct answers, and the average score is simply 1/n,
+        where n is the number of boxes (the same is true for the quadratic version):
+
+            ┌───────────────────┬───────┬───────┬───────┬───────┐
+            │   number of boxes │   4   │   6   │   8   │  10   │
+            ├───────────────────┼───────┼───────┼───────┼───────┤
+            │   average score   │ 0.25  │ 0.17  │ 0.13  │ 0.10  │
+            └───────────────────┴───────┴───────┴───────┴───────┘
+
+        If the proportion of correct answers is less than 0.5, as is typically the case,
         return the difference between the number of correctly checked answers
         and the number of incorrectly checked answers, divided by the total number
-        of correct answers.
+        of correct answers if any.
 
         Otherwise, if the proportion of correct answers is greater than 0.5,
         return the difference between the number of correctly unchecked answers
         and the number of incorrectly unchecked answers, divided by the total number
-        of incorrect answers.
+        of incorrect answers if any.
 
         The idea behind this algorithm is that, if the proportion of correct answers
         is greater than 0.5, the exercise's difficulty lies in determining
         which answers should be left unchecked.
 
-        We then raise the result to the power of `exponent`, and keep it in a [-1,1] range.
+        This value will be eventually floored to keep it in the range [-1,1].
 
         If the result is positive, return result * score.correct.
-        Else, return -result * score.incorrect.
+        Else, return -abs(result) * score.incorrect.
         """
-        return _legacy_correct_minus_incorrect(answers, score)
+        return _special_correct_minus_incorrect(answers, score)
 
     @staticmethod
-    def legacy_correct_minus_incorrect_quadratic(answers: AnswersData, score: ScoreData) -> float:
-        """Same algorithm as for `legacy_correct_minus_incorrect`, but the result is squared.
+    def special_correct_minus_incorrect_quadratic(answers: AnswersData, score: ScoreData) -> float:
+        """Same algorithm as for `special_correct_minus_incorrect`, but the ratio are squared.
 
         This makes more unlikely for a student answering randomly to gain significant score.
         """
-        return _legacy_correct_minus_incorrect(answers, score, exponent=2)
+        return _special_correct_minus_incorrect(answers, score, exponent=2)
 
     @staticmethod
     def correct_minus_incorrect(answers: AnswersData, score: ScoreData) -> float:
         """Return the proportion of checked answers among correct ones, minus the proportion of
         checked answers between incorrect ones.
+
+        This algorithm works better when minimal score is the opposite of the maximal score
+        (-1 and +1 for example), since the average score when answering randomly will be 0.
 
         If there are no correct answers, or if all answers are correct, the previous formula
         can't obviously be applied.
@@ -181,7 +212,7 @@ class EvaluationStrategies:
 
     @staticmethod
     def correct_minus_incorrect_quadratic(answers: AnswersData, score: ScoreData) -> float:
-        """Same algorithm as for `correct_minus_incorrect`, but the proportions' difference is squared.
+        """Same algorithm as for `correct_minus_incorrect`, but the ratio difference is squared.
 
         This makes more unlikely for a student answering randomly to gain significant score.
         """
@@ -199,51 +230,28 @@ def _checked_among_correct_proportion(answers: AnswersData) -> float:
 
 
 def _partial_answers(answers: AnswersData, score: ScoreData, exposant=1.0) -> float:
-    """Return `score.incorrect` if an incorrect answer was checked, else the proportion of correct answers."""
+    """See: `EvaluationStrategies.partial_answers`."""
     if answers.checked & answers.incorrect:
         return score.incorrect
     else:
         return round(_checked_among_correct_proportion(answers) ** exposant * score.correct, 2)
 
 
-def _legacy_correct_minus_incorrect(answers: AnswersData, score: ScoreData, exponent=1.0) -> float:
-    """If the proportion of correct answers is less than 0.5, as is typically the case,
-    return the difference between the number of correctly checked answers
-    and the number of incorrectly checked answers, divided by the total number
-    of correct answers.
-
-    Otherwise, if the proportion of correct answers is greater than 0.5,
-    return the difference between the number of correctly unchecked answers
-    and the number of incorrectly unchecked answers, divided by the total number
-    of incorrect answers.
-
-    The idea behind this algorithm is that, if the proportion of correct answers
-    is greater than 0.5, the exercise's difficulty lies in determining
-    which answers should be left unchecked.
-
-    We then raise the result to the power of `exponent`, and keep it in a [-1,1] range.
-
-    If the result is positive, return result * score.correct.
-    Else, return -result * score.incorrect.
-    """
+def _special_correct_minus_incorrect(answers: AnswersData, score: ScoreData, exponent=1.0) -> float:
+    """See: `EvaluationStrategies.special_correct_minus_incorrect`."""
+    if score.incorrect > 0:
+        raise IncorrectScoreParameter(
+            f"The score for an incorrect answer must be zero or negative, yet {score.incorrect} > 0."
+        )
     diff = 1 - len(answers.incorrectly_answered) / max(1, min(len(answers.correct), len(answers.incorrect)))
-    diff = max(-1, diff**exponent)
-    assert -1 <= diff <= 1
-    result = diff * score.correct if diff > 0 else -diff * score.incorrect
+    diff = max(-1.0, diff)
+    assert -1 <= diff <= 1, diff
+    result = abs(diff) ** exponent * (score.correct if diff > 0 else score.incorrect)
     return round(result, 2)
 
 
 def _correct_minus_incorrect(answers: AnswersData, score: ScoreData, exponent=1.0) -> float:
-    """Return the proportion of checked answers among correct ones, minus the proportion of
-    checked answers between incorrect ones.
-
-    If there are no correct answers, or if all answers are correct, the previous formula
-    can't obviously be applied.
-    In that case, the score is only based on the proportion of rightly checked or unchecked
-    answers among all answers:
-      - if this proportion is higher than 1/2, then the result will be a fraction of `score.correct`.
-      - else, the result will be a fraction of `score.incorrect`.
-    """
+    """See: `EvaluationStrategies.correct_minus_incorrect`."""
     if score.incorrect > 0:
         raise IncorrectScoreParameter(
             f"The score for an incorrect answer must be zero or negative, yet {score.incorrect} > 0."
@@ -265,7 +273,9 @@ def _correct_minus_incorrect(answers: AnswersData, score: ScoreData, exponent=1.
 
     else:
         correct_ratio = len(answers.checked & answers.correct) / len(answers.correct)
+        assert correct_ratio <= 1
         incorrect_ratio = len(answers.checked & answers.incorrect) / len(answers.incorrect)
-        diff = (correct_ratio - incorrect_ratio) ** exponent
+        assert incorrect_ratio <= 1
+        diff = correct_ratio - incorrect_ratio
         assert -1 <= diff <= 1
-        return diff * score.correct if diff > 0 else -diff * score.incorrect
+        return abs(diff) ** exponent * (score.correct if diff > 0 else score.incorrect)
--- tests/test_evaluation_strategies.py
+++ tests/test_evaluation_strategies.py
@@ -1,3 +1,5 @@
+import random
+
 from ptyx_mcq.scan.evaluation_strategies import EvaluationStrategies, AnswersData, ScoreData
 
 
@@ -5,44 +7,44 @@ from ptyx_mcq.scan.evaluation_strategies import EvaluationStrategies, AnswersDat
 def test_legacy_correct_minus_incorrect_1():
     scores = ScoreData(correct=6, skipped=0, incorrect=-1)
     answers = AnswersData(checked={1, 2}, correct={1, 2}, all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 6
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 6
     answers = AnswersData(checked={1, 2}, correct={1}, all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 0
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 0
     answers = AnswersData(checked={1, 2}, correct=set(), all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == -1
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == -1
     answers = AnswersData(checked=set(), correct=set(), all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 6
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 6
     answers = AnswersData(checked={1}, correct={1, 2}, all={1, 2, 3, 4})
     # half the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 3
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 3
     answers = AnswersData(checked={1, 3}, correct={1, 2, 3}, all={1, 2, 3, 4, 5, 6})
     # 2/3 of the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 4
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 4
     answers = AnswersData(checked={1, 2, 4}, correct={1, 2, 3}, all={1, 2, 3, 4, 5, 6})
     # 1/3 of the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 2
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 2
 
 
 # noinspection PyTypeChecker
 def test_legacy_correct_minus_incorrect_0():
     scores = ScoreData(correct=6, skipped=0, incorrect=0)
     answers = AnswersData(checked={1, 2}, correct={1, 2}, all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 6
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 6
     answers = AnswersData(checked={1, 2}, correct={1}, all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 0
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 0
     answers = AnswersData(checked={1, 2}, correct=set(), all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 0
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 0
     answers = AnswersData(checked=set(), correct=set(), all={1, 2, 3, 4})
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 6
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 6
     answers = AnswersData(checked={1}, correct={1, 2}, all={1, 2, 3, 4})
     # half the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 3
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 3
     answers = AnswersData(checked={1, 3}, correct={1, 2, 3}, all={1, 2, 3, 4, 5, 6})
     # 2/3 of the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 4
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 4
     answers = AnswersData(checked={1, 2, 4}, correct={1, 2, 3}, all={1, 2, 3, 4, 5, 6})
     # 1/3 of the score
-    assert EvaluationStrategies.legacy_correct_minus_incorrect(answers=answers, score=scores) == 2
+    assert EvaluationStrategies.special_correct_minus_incorrect(answers=answers, score=scores) == 2
 
 
 # noinspection PyTypeChecker
@@ -76,3 +78,100 @@ def test_correct_minus_incorrect():
     assert EvaluationStrategies.correct_minus_incorrect(answers=answers, score=scores) == 0
     answers = AnswersData(checked={1, 2, 3, 4}, correct=set(), all={1, 2, 3, 4})
     assert EvaluationStrategies.correct_minus_incorrect(answers=answers, score=scores) == -2
+
+
+def rand_subset(s: set, size: int = None) -> set:
+    if size is None:
+        return {elt for elt in s if random.random() < 0.5}
+    elif size == len(s):
+        return s.copy()
+    elif size > len(s):
+        raise ValueError(
+            f"Incorrect size ({size} > {len(s)}). A subset can't be larger than the original set."
+        )
+    return set(random.choices(list(s), k=size))
+
+
+# noinspection PyTypeChecker
+def get_random_answers_data(
+    number_of_answers: int = 6,
+    number_of_checked_answers: int = None,
+    number_of_correct_answers: int = None,
+) -> AnswersData:
+    all_answers = set(range(number_of_answers))
+    correct_answers = rand_subset(all_answers, size=number_of_correct_answers)
+    checked_answers = rand_subset(all_answers, size=number_of_checked_answers)
+    return AnswersData(checked=checked_answers, correct=correct_answers, all=all_answers)  # type: ignore
+
+
+# noinspection PyTypeChecker
+def random_evaluation(
+    strategy: str,
+    scores: ScoreData,
+    number_of_answers: int = 6,
+    number_of_checked_answers: int | float | None = None,
+    number_of_correct_answers: int | float | None = None,
+    loops: int = 10000,
+):
+    if isinstance(number_of_checked_answers, float) and 0 < number_of_checked_answers < 1:
+        number_of_checked_answers = int(number_of_checked_answers * number_of_answers)
+    if not (isinstance(number_of_checked_answers, int) or number_of_checked_answers is None):
+        raise ValueError(number_of_checked_answers)
+    if isinstance(number_of_correct_answers, float) and 0 < number_of_correct_answers < 1:
+        number_of_correct_answers = int(number_of_correct_answers * number_of_answers)
+    if not (isinstance(number_of_correct_answers, int) or number_of_correct_answers is None):
+        raise ValueError(number_of_correct_answers)
+    total = 0
+    for i in range(loops):
+        answers = get_random_answers_data(
+            number_of_answers=number_of_answers,
+            number_of_checked_answers=number_of_checked_answers,
+            number_of_correct_answers=number_of_correct_answers,
+        )
+        total += getattr(EvaluationStrategies, strategy)(answers=answers, score=scores)
+    return 100 * total / loops
+
+
+# noinspection PyTypeChecker
+def test_random_evaluation():
+    """This is used mainly for simulations, and is not meant for unit tests."""
+    # ---------------------------------------------------------------
+    # Parameters:
+    correct = 1
+    incorrect = 0
+    loops = 1000  # at least 1000 for sensible results
+    # `number_of_checked_answers` and `number_of_correct_answers` values can be either :
+    # - `None`: the number of checked/correct answers will be set each time randomly
+    # - an integer: the number of checked/correct answers
+    # - a float between 0 and 1: the proportion of checked/correct answers among all answers
+    number_of_checked_answers = None
+    number_of_correct_answers = None
+    # ---------------------------------------------------------------
+    random.seed(594135875)
+    scores = ScoreData(correct=correct, skipped=0, incorrect=incorrect)
+
+    for strategy in (
+        "special_correct_minus_incorrect",
+        "special_correct_minus_incorrect_quadratic",
+        "correct_minus_incorrect",
+        "correct_minus_incorrect_quadratic",
+    ):
+
+        def print_results(number_of_answers):
+            print(
+                random_evaluation(
+                    strategy,
+                    scores=scores,
+                    number_of_answers=number_of_answers,
+                    number_of_checked_answers=number_of_checked_answers,
+                    number_of_correct_answers=number_of_correct_answers,
+                    loops=loops,
+                )
+            )
+
+        print()
+        print(f"Strategy {strategy}:")
+        print_results(number_of_answers=4)
+        print_results(number_of_answers=6)
+        print_results(number_of_answers=8)
+        print_results(number_of_answers=10)
