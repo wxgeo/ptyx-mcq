d236689aeaf74946d9a6f4c3db82dad9cfb7405f
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2024-10-25 09:50:55 +0200

    test: Add tests to test_is_ptyx_file()

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,5 @@
+import shutil
+
 import pytest
 
 from ptyx_mcq.tools.extend_literal_eval import extended_literal_eval
@@ -18,9 +20,24 @@ def test_extend_literal_eval():
     assert not (a == [1, 2, float("inf")])  # Do not test with !=, since it is not reimplemented.
 
 
-def test_is_ptyx_file():
-    assert is_ptyx_file(TEST_DIR / "data/ptyx-files/minimal-working-example/minimal-working-example.ptyx")
-    assert not is_ptyx_file(TEST_DIR / "data/ptyx-files/minimal-working-example/minimal-working-example.tex")
+def test_is_ptyx_file(tmp_path):
+    # pTyX files must be accepted
+    ptyx_file = TEST_DIR / "data/ptyx-files/minimal-working-example/minimal-working-example.ptyx"
+    assert ptyx_file.is_file()
+    assert is_ptyx_file(ptyx_file)
+    # LateX files must be rejected
+    latex_file = ptyx_file.with_suffix(".tex")
+    assert latex_file.is_file()
+    assert not is_ptyx_file(latex_file)
+    # Non-existent files must be rejected
+    nonexistent_file = ptyx_file.with_suffix(".nonexistent")
+    assert not nonexistent_file.is_file()
+    assert not is_ptyx_file(nonexistent_file)
+    # pTyX files with incorrect extensions must yet be accepted
+    incorrect_extension_file = tmp_path / "wrong-extension.extension"
+    shutil.copy(ptyx_file, incorrect_extension_file)
+    assert incorrect_extension_file.is_file()
+    assert is_ptyx_file(incorrect_extension_file)
 
 
 def test_get_file_with_extension():
