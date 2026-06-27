8de5e602214767cbec0279e4d679d8896110cc40
Nicolas Pourcelot <nicolas.pourcelot@gmail.com>
2026-06-27 17:23:43 +0200

    feat: More explicit error messages when the config could not be updated.

--- src/ptyx_mcq/other_commands/update.py
+++ src/ptyx_mcq/other_commands/update.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 from ptyx.latex_generator import Compiler
-from ptyx.pretty_print import print_error, print_success
-
+from ptyx.pretty_print import print_error, print_success, print_warning, print_info
 from ptyx_mcq.make.include_directives.update import update_file
 from ptyx_mcq.parameters import CONFIG_FILE_EXTENSION
 from ptyx_mcq.scan.data import ScanData
@@ -39,38 +38,57 @@ def update_config_file(path: Path) -> None:
     # The number of questions and their ordering should not have changed,
     # the same holds for the answers (but their labeling as correct
     # or incorrect is allowed to change).
-    if not same_questions_and_answers_numbers(data, config):
+    output_path = config_file.parent / (config_file.name.split(".")[0] + CONFIG_FILE_EXTENSION)
+    ok, msg = same_questions_and_answers_numbers(data, config)
+    if not ok:
         cmd = f"mcq make {path}"
         sep = len(cmd) * "-"
         cmd = f"{sep}\n{cmd}\n{sep}\n"
-        print_error("Questions or answers number changed.\nYou must run a full compilation:\n" + cmd)
+        if msg:
+            print_warning(msg)
+        print_error(
+            "The number or ordering of the questions or answers changed.\n"
+            "You must run a full compilation (you should make a backup of the folder first):\n" + cmd
+        )
+        data.dump(failed_output_path := output_path.with_suffix(".fail"))
+        print_info(
+            f"The failed attempt was stored in {failed_output_path.name!r}.\n"
+            "You can inspect it with the following command:\n"
+            f"diff '{output_path}' '{failed_output_path}' | less -R"
+        )
         raise FatalError
 
     # Get back the positions of checkboxes from last full compilation.
     data.id_table_pos = config.id_table_pos
     data.boxes = config.boxes
-    data.dump(config_file.parent / (config_file.name.split(".")[0] + CONFIG_FILE_EXTENSION))
+    data.dump(output_path)
     # Students names' cached values must be updated, because the names may have been updated in the config file.
     update_cached_names(config_file)
     print_success("Configuration file was successfully updated.")
 
 
-def same_questions_and_answers_numbers(config1: Configuration, config2: Configuration) -> bool:
+def same_questions_and_answers_numbers(config1: Configuration, config2: Configuration) -> tuple[bool, str]:
     """Check that the number of questions and answers per question is the same in both config files."""
-    if list(config1.ordering) != list(config2.ordering):
-        return False
+    delta_doc = set(config1.ordering).symmetric_difference(set(config2.ordering))
+    if delta_doc:
+        return False, f"Those documents are found only in one configuration and not the other: {delta_doc}."
     for doc_id in config1.ordering:
         ordering1 = config1.ordering[doc_id]
         ordering2 = config2.ordering[doc_id]
         if ordering1["questions"] != ordering2["questions"]:
-            return False
+            return False, f"The number of questions or their order changed for document {doc_id} (at least)."
         if list(ordering1["answers"]) != list(ordering2["answers"]):
-            return False
+            # Should not occur, either a bug or a corrupted configuration file (maybe because of a manual edition).
+            return False, f"Incoherent questions data for document {doc_id} (at least)."
 
         for q in ordering1["answers"]:
             if [a for a, _ in ordering1["answers"][q]] != [a for a, _ in ordering2["answers"][q]]:
-                return False
-    return True
+                return (
+                    False,
+                    f"The number of answers or their order changed for the question {q}"
+                    " of the document {doc_id} (at least).",
+                )
+    return True, ""
 
 
 def update_exercises(path: Path, force=False, clean=False) -> None:
